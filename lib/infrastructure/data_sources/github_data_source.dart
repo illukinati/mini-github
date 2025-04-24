3a850a921b194b0405e7abb62b54da93c9a9e333
エコ　プラセティオ <ekoprasetyolnh@gmail.com>
2025-04-24 11:03:15 +0900

    feat: Add GitHub token for authentication and improve error handling
    This commit introduces the use of a temporary GitHub token for authentication and enhances error handling in `GithubDataSource`.
    
    **Key changes:**
    
    -   **Token Integration:**
        -   Added `MyConstant` class to store a temporary GitHub token.
        -   Included the token in request headers for authentication in `getAllUsers`, `getUser`, and `getUserRepos`.
        - Added comment that hard-coding a token like this is only for testing.
    -   **Error Handling:**
        -   Modified `CustomFailure` messages to include "Dio Exception" or "Catch Exception".
        - Added token to headers for every network request.

--- lib/infrastructure/data_sources/github_data_source.dart
+++ lib/infrastructure/data_sources/github_data_source.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:mini_github/domain/values/failure_state.dart';
+import 'package:mini_github/infrastructure/core/constant.dart';
 import 'package:mini_github/infrastructure/core/urls.dart';
 import 'package:mini_github/infrastructure/models/repo_model.dart';
 import 'package:mini_github/infrastructure/models/user_model.dart';
@@ -10,14 +11,19 @@ class GithubDataSource {
     try {
       final dio = Dio();
       Response response;
-      response = await dio.get(MyUrl.allUsersUrl);
+      response = await dio.get(
+        MyUrl.allUsersUrl,
+        options: Options(
+          headers: {'Authorization': 'token ${MyConstant.myToken}'},
+        ),
+      );
       final data = response.data;
       List<UserModel> users = data.map((e) => UserModel.fromJson(e)).toList();
       return right(users);
     } on DioException catch (e) {
-      return left(CustomFailure(e.toString()));
+      return left(CustomFailure("Dio Exception: $e"));
     } catch (e) {
-      return left(CustomFailure(e.toString()));
+      return left(CustomFailure("Catch Exception: $e"));
     }
   }
 
@@ -25,7 +31,12 @@ class GithubDataSource {
     try {
       final dio = Dio();
       Response response;
-      response = await dio.get(MyUrl.userDetailUrl(username));
+      response = await dio.get(
+        MyUrl.userDetailUrl(username),
+        options: Options(
+          headers: {'Authorization': 'token ${MyConstant.myToken}'},
+        ),
+      );
       final data = response.data;
       UserModel user = UserModel.fromJson(data);
       return right(user);
@@ -42,7 +53,12 @@ class GithubDataSource {
     try {
       final dio = Dio();
       Response response;
-      response = await dio.get(MyUrl.userReposUrl(username));
+      response = await dio.get(
+        MyUrl.userReposUrl(username),
+        options: Options(
+          headers: {'Authorization': 'token ${MyConstant.myToken}'},
+        ),
+      );
       final data = response.data;
       List<RepoModel> repos = data.map((e) => RepoModel.fromJson(e)).toList();
       return right(repos);
